585e04ce62dc840462c600c3973995f0becb46e8
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-12 18:35:38 +0200

    *

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,75 +1,39 @@
-use crate::beytecode;
-pub struct Memory {
-  pub ram: [u8; 0xFFFF],
-  pub start_address: usize,
-}
+use crate::memory::Memory;
+use crate::opcode;
 
-impl Memory {
-  pub fn new() -> Memory {
-    Memory { ram: [0; 0xFFFF], start_address: 0x8000 }
-  }
-  pub fn read(&self, address: u16) -> u8 {
-    self.ram[address as usize]
-  }
-  pub fn write(&mut self, address: u16, value: u8) {
-    self.ram[address as usize] = value;
-  }
-
-  pub fn load_program(&mut self, program: Vec<u8>) -> u16 {
-    // let start_address: usize = 0x8000;
-    // Load program into memory starting at 0x8000
-    self.ram[self.start_address..(self.start_address + program.len())].copy_from_slice(&program[..]);
-    // Set the program counter to the start address of the program
-    self.write_u16(0xFFFC, self.start_address as u16);
-    // Return the start address of the program
-    return self.start_address as u16;
-  }
-
-  /*
-
-  In NES CPU uses little-endian memory addressing;
-  This means that the least significant byte of a multi-byte value is stored at the lowest memory address.
-  For example:
-  Real Memory Address: 0x8000 (big-endian)
-  big-endian: 80 00
-  little-endian: 00 80
-  This is the opposite of big-endian memory addressing, where the most significant byte is stored at the lowest memory address.
-  ------------
-  little-endian (aka big-endian): https://en.wikipedia.org/wiki/Endianness
-
-
-  */
-  fn read_u16(&mut self, pos: u16) -> u16 {
-    let lo = self.read(pos) as u16;
-    let hi = self.read(pos + 1) as u16;
-    (hi << 8) | (lo as u16)
-  }
-  fn write_u16(&mut self, pos: u16, data: u16) {
-    let hi = (data >> 8) as u8;
-    let lo = (data & 0xff) as u8;
-    self.write(pos, lo);
-    self.write(pos + 1, hi);
-  }
-}
 pub struct CPU {
   pub program_counter: u16,
   pub register_a: u8,
   pub register_x: u8,
   pub status: u8,
-  pub memory: Memory,
+  pub mem: Memory,
+}
+
+pub enum AddressingMode {
+  Immediate,
+  ZeroPage,
+  ZeroPageX,
+  ZeroPageY,
+  Absolute,
+  AbsoluteX,
+  AbsoluteY,
+  IndirectX,
+  IndirectY,
+  NoneAddressing,
 }
 
 impl Default for CPU {
   fn default() -> Self {
-    CPU { program_counter: 0, register_a: 0, register_x: 0, status: 0, memory: Memory::new() }
+    CPU { program_counter: 0, register_a: 0, register_x: 0, status: 0, mem: Memory::new() }
   }
 }
+
 impl CPU {
-  pub fn new(memory: Memory) -> Self {
-    CPU { program_counter: 0, register_a: 0, register_x: 0, status: 0, memory }
+  pub fn new(mem: Memory) -> Self {
+    CPU { program_counter: 0, register_a: 0, register_x: 0, status: 0, mem }
   }
   pub fn boot(&mut self, program: Vec<u8>) {
-    self.memory.load_program(program);
+    self.mem.load_program(program);
     self.reset();
     self.run()
   }
@@ -78,20 +42,65 @@ impl CPU {
     // self.register_a = 0;
     // self.register_x = 0;
     // self.status = 0;
-    self.program_counter = self.memory.read_u16(0xFFFC);
+    self.program_counter = self.mem.read_u16(0xFFFC);
     println!("PC: 0x{:04X}", self.program_counter);
+  }
+
+  pub fn get_operand_address(&mut self, addressing_mode: AddressingMode) -> u16 {
+    match addressing_mode {
+      AddressingMode::Immediate => self.program_counter,
+      AddressingMode::ZeroPage => self.mem.read(self.program_counter) as u16,
+      AddressingMode::Absolute => self.mem.read_u16(self.program_counter),
+
+      AddressingMode::ZeroPageX => self.zero_page_address(self.register_x),
+      // AddressingMode::ZeroPageY => self.zero_page_address().wrapping_add(self.register_y as u16),
+      AddressingMode::AbsoluteX => self.absolute_address(self.register_x),
+      AddressingMode::AbsoluteY => self
+        .mem
+        .read_u16(self.program_counter)
+        .wrapping_add(self.register_y as u16),
+      AddressingMode::IndirectX => {
+        let zero_page_address = self.mem.read(self.program_counter).wrapping_add(self.register_x) as u16;
+        self.mem.read_u16(zero_page_address)
+      }
+      AddressingMode::IndirectY => {
+        let zero_page_address = self.mem.read(self.program_counter) as u16;
+        self
+          .mem
+          .read_u16(zero_page_address)
+          .wrapping_add(self.register_y as u16)
+      }
+      AddressingMode::NoneAddressing => 0,
+    }
+  }
+
+  pub fn zero_page_address(&mut self, adresse: u8) -> u16 {
+    let base = self.mem.read(self.program_counter) as u16;
+    let adress = base.wrapping_add(adresse as u16) as u16;
+    adress
+  }
+
+  pub fn absolute_address(&mut self, adresse: u8) -> u16 {
+    let base = self.mem.read_u16(self.program_counter);
+    let adress = base.wrapping_add(adresse as u16) as u16;
+    adress
+  }
+
+  pub fn indirect_address(&mut self) -> u16 {
+    let pos = self.mem.read_u16(self.program_counter);
+    let opr
   }
 
   pub fn run(&mut self) {
     loop {
-      let opcode = self.memory.read(self.program_counter);
+      let opcode = self.mem.read(self.program_counter);
       self.program_counter += 1;
       println!("Opcode: 0x{:02X}", opcode);
       match opcode {
-        beytecode::OPCODE_LDA => self.load_accumulator(),
-        beytecode::OPCODE_BRK => return,
-        beytecode::OPCODE_TAX => self.transfer_accumulator(),
-        beytecode::OPCODE_INX => self.increment_register(),
+        opcode::OPCODE_LDA => self.load_accumulator(),
+        opcode::OPCODE_BRK => return,
+        opcode::OPCODE_TAX => self.transfer_accumulator(),
+        opcode::OPCODE_INX => self.increment_register(),
         _ => {
           println!("Unrecognized opcode: {}", opcode);
           break;
@@ -101,7 +110,7 @@ impl CPU {
   }
 
   fn load_accumulator(&mut self) {
-    let value = self.memory.read(self.program_counter);
+    let value = self.mem.read(self.program_counter);
     self.program_counter = self.program_counter.wrapping_add(1);
     self.register_a = value;
     self.update_zero_and_negative_flags(self.register_a);
@@ -122,15 +131,15 @@ impl CPU {
   // helper functions...
   fn update_zero_and_negative_flags(&mut self, register: u8) {
     if register == 0 {
-      self.status |= beytecode::HEX_2; // Set zero flag if A is zero
+      self.status |= opcode::HEX_2; // Set zero flag if A is zero
     } else {
-      self.status &= beytecode::HEX_253; // Clear zero flag if A is not zero
+      self.status &= opcode::HEX_253; // Clear zero flag if A is not zero
     }
     // Check if bit 7 is set
-    if register & beytecode::HEX_128 != 0 {
-      self.status |= beytecode::HEX_128; // Set negative flag if bit 7 is set
+    if register & opcode::HEX_128 != 0 {
+      self.status |= opcode::HEX_128; // Set negative flag if bit 7 is set
     } else {
-      self.status &= beytecode::HEX_127; // Clear negative flag if bit 7 is not set
+      self.status &= opcode::HEX_127; // Clear negative flag if bit 7 is not set
     }
   }
 }
